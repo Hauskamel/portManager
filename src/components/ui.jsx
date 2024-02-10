cbf0c36d877b74e2d4bcde2c8f957ba9c1b006ef
Hauskamel <lukas.krojer@gmx.de>
2024-02-10 11:59:35 +0100

    inventory ui

--- src/components/ui.jsx
+++ src/components/ui.jsx
@@ -3,31 +3,24 @@ import {useState} from "react";
 // import endYear from "../functions/end-year.js";
 
 function Ui (props) {
+
+    // Conditions
     let YEAR = props.data.data.year;
     let MONEY = props.data.data.money;
     let SHIPSOWNED = props.data.data.shipsOwned
     let DOCKS = props.data.data.docks
 
-    // in tons
-    let INVENTORY = {
-        tea: 100,
-        grain: 100,
-        spices: 0,
-        slaves: 200,
-        preciousMetals: 50
-    }
-
-    const [year, setYear] = useState(YEAR)
-
-    function endYear () {
-        YEAR = setYear( year + 1);
-    }
+    // Inventory
+    let WOOL = props.data.inventory.wool
+    let TEXTILES = props.data.inventory.textiles
 
     return (
         <div className="ui-container">
 
             <div className="ui-inventory">
                 <h3>Your Inventory:</h3>
+                <p>Wool: {WOOL}</p>
+                <p>Textiles: {TEXTILES}</p>
             </div>
 
             <div className="ui-stats">
@@ -35,7 +28,7 @@ function Ui (props) {
                 <h3 className="money">Money: {MONEY}</h3>
                 <h3 className="shipsOwned">Ships you own: {SHIPSOWNED}</h3>
                 <h3 className="docks">Docks: {DOCKS}</h3>
-                <button onClick={endYear} className="end_round_btn">End round</button>
+                <button className="end_round_btn">End round</button>
             </div>
         </div>
     )
